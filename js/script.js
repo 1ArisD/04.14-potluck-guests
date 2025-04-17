a3897b9ece874fa74466d43ecfc6bbe950743039
arisd <arisdunn@gmail.com>
2025-04-16 19:58:52 -0500

    Assign dishes

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full
 const guestFull = document.querySelector(".alert");
+//
+const assignButton = document.querySelector(".assigned");
+//
+const assignedItems = document.querySelector(".assigned-items");
 
 addGuestButton.addEventListener("click", function () {
   const guest = guestInput.value;
@@ -40,5 +44,30 @@ const updateGuestCount = function () {
     guestInput.classList.add("hide");
     guestInputLabel.classList.add("hide");
     guestFull.classList.remove("hide");
+  }
+};
+
+const assignItems = function () {
+  const potluckItems = [
+    "cheese tray",
+    "fruit platter",
+    "chicken wings",
+    "meatballs",
+    "pico de gallo",
+    "guacamole",
+    "queso",
+    "tortilla chips",
+    "punch",
+    "lemonade",
+    "cookies",
+    "brownies",
+  ];
+  const allGuests = document.querySelectorAll(".guest-list li");
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem);
   }
 };
